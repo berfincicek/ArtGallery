9d5f75aba29c9054478a38f15d38bf18246a2703
Berfin Çiçek <cicek_berfin@hotmail.com>
2022-01-02 21:25:07 +0300

    init function added

--- art_gallery.js
+++ art_gallery.js
@@ -8,38 +8,47 @@
 // Description: js file of the art gallery implemented by three.js
 //-------------------------------------------------
 
+let scene, camera, renderer, cube, sphere, dodecahedron,skyboxGeo;
 
-    //initialize scene
-    const scene = new THREE.Scene();
-	scene.background = new THREE.Color( 0xFCE5CD );
+    function init(){
+        //initialize scene
+        scene = new THREE.Scene();
+        scene.background = new THREE.Color( 0xFCE5CD );
 
-    //initialize camera
-    const camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.1, 1000 );
+        //initialize camera
+        camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-    const renderer = new THREE.WebGLRenderer();
-    renderer.setSize( window.innerWidth, window.innerHeight/*, false*/ ); // commented part is for lower resolution
-    document.body.appendChild( renderer.domElement );
+        renderer = new THREE.WebGLRenderer({ antialias: true });
+        renderer.setSize( window.innerWidth, window.innerHeight/*, false*/ ); // commented part is for lower resolution
+        document.body.appendChild( renderer.domElement );
 
-    const geometry = new THREE.BoxGeometry();
-    //const material = new THREE.MeshBasicMaterial( { color: 0xC27BA0 } );
-    const material = new THREE.MeshNormalMaterial({shading : THREE.FlatShading}); //every side has different colors
-    const cube = new THREE.Mesh( geometry, material );
-    scene.add( cube );
+        skyboxGeo = new THREE.BoxGeometry(10000, 10000, 10000);
+        skybox = new THREE.Mesh(skyboxGeo);
+        scene.add(skybox);
 
-    const radius = 1;
-    const geometry2 = new THREE.DodecahedronGeometry(radius);
-    const geometry3 = new THREE.SphereGeometry();
+        const geometry = new THREE.BoxGeometry();
+        //const material = new THREE.MeshBasicMaterial( { color: 0xC27BA0 } );
+        const material = new THREE.MeshNormalMaterial({shading : THREE.FlatShading}); //every side has different colors
+        cube = new THREE.Mesh( geometry, material );
+        scene.add( cube );
 
-    const dodecahedron = new THREE.Mesh( geometry2, material );
-    dodecahedron.position.x=2;
-    scene.add(dodecahedron);
+        const radius = 1;
+        const geometry2 = new THREE.DodecahedronGeometry(radius);
+        const geometry3 = new THREE.SphereGeometry();
 
-    const sphere = new THREE.Mesh( geometry3, material );
-    sphere.position.x=-2;
-    scene.add(sphere);
+        dodecahedron = new THREE.Mesh( geometry2, material );
+        dodecahedron.position.x=2;
+        scene.add(dodecahedron);
 
-    camera.position.z = 5;
+        sphere = new THREE.Mesh( geometry3, material );
+        sphere.position.x=-2;
+        scene.add(sphere);
 
+        camera.position.z = 5;
+        //camera.position.set(1200, -250, 20000);
+
+        animate();
+    }
     function animate() {
         requestAnimationFrame( animate );
 
@@ -52,8 +61,12 @@
         sphere.rotation.x += 0.01;
         sphere.rotation.y += 0.01;
 
+        skybox.rotation.x += 0.005;
+        skybox.rotation.y += 0.005;
+
+  //requestAnimationFrame(animate);
 
         renderer.render( scene, camera );
     };
 
-    animate();
+init();
